cccbeb6586c8965ceda5170a927988c2eb077293
SLITI Brahim <brahim.sliti@supinfo.com>
2017-12-20 15:49:05 +0100

    Adding commands interpretation

--- header.h
+++ header.h
@@ -5,6 +5,7 @@
 
 typedef struct Task Task;
 struct Task {
+    // unsigned id;
     char name[50];
     char desc[100];
     bool isDone;
@@ -12,5 +13,9 @@ struct Task {
 };
 
 // Function Declarations
+void displayWelcome();
+void userInput(int *ptInProgress);
 void debugTask(struct Task task);
 void initTask(struct Task *task);
+void clearBuffer(char *command);
+void interpret(char *command, int* ptInProgress);
--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include "header.h"
 
 int main() {
-    Task sport;
+    // Task sport;
 
+    // initTask(&sport);
     // debugTask(sport);
-    initTask(&sport);
-    debugTask(sport);
+
+    displayWelcome();
+    int inProgress = 1;
+    while (inProgress) {
+        userInput(&inProgress);
+    }
+
+    return 0;
+}
+
+void displayWelcome() {
+    printf("ToDo List in C\n\n");
+}
+
+void userInput(int* ptInProgress) {
+
+    printf("\nEnter Command: (help for commands) ");
+    char command[100];
+    fgets(command, sizeof(command), stdin);
+    clearBuffer(command);
+
+    // printf("Result: %s", command);
+    interpret(command, ptInProgress);
 }
 
 void debugTask(struct Task task) {
@@ -22,4 +44,48 @@ void initTask(struct Task *ptTask) {
     strcpy(ptTask->name, "Undefined");
     ptTask->isDone = false;
     strcpy(ptTask->date, "Undefined");
+}
+
+void clearBuffer(char* command) {
+    
+    char* p;
+
+    p = strchr(command, '\n');
+    if (p) {
+        *p = '\0';
+    } else { while((getchar() != '\n') && (getchar() != EOF)); }
+}
+
+void helpDisplay() {
+    printf("\ncreate:  create new task\n");
+    printf("help:  display available commands\n");
+    printf("done:  mark a specified task as done\n");
+    printf("undone:  mark a specified task as undone\n");
+    printf("exit:  Exit the program\n");
+}
+
+void interpret(char* command, int* ptInProgress) {
+
+    if(strcmp("create", command) == 0) {
+        printf("create");
+    }
+    else if(strcmp("help", command) == 0) {
+        helpDisplay();
+    }
+    else if (strcmp("done", command) == 0)
+    {
+        printf("done");
+    }
+    else if (strcmp("undone", command) == 0)
+    {
+        printf("undone");
+    }
+    else if (strcmp("exit", command) == 0)
+    {
+        printf("\nThank you for having use my ToDo List\n");
+        *ptInProgress = 0;
+    }
+    else {
+        printf("Invalid Command, please type 'help' for displaying available commands");
+    }
 }
